139fb53f5af3273e7a9c55d88e280fd4a6df76f8
Username <user@example.com>
2020-12-08 08:41:18 +0100

    8.1 et 8.2

---
+++ 8/pgm.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+def execute(pgm):
+    pc = 0
+    acc = 0
+    s = set([0])
+
+    while True:
+        cmd, val = pgm[pc]
+        val = int(val)
+        if cmd == "acc":
+            acc += val
+            pc += 1
+        elif cmd == "jmp":
+            pc += val
+        elif cmd == "nop":
+            pc += 1
+        if pc == len(pgm):
+            return True, acc
+        if pc in s:
+            return False, acc
+        s.add(pc)
+
+def main():
+    with open("input.txt") as f:
+        pgm = [x.strip().split() for x in f.readlines()]
+
+    _, val = execute(pgm)
+    print(val)
+
+    for i in range(len(pgm)):
+        cmd, val = pgm[i]
+        val = int(val)
+        if cmd == "jmp":
+            l = pgm.copy()
+            l[i] = ["nop", val]
+        elif cmd == "nop":
+            l = pgm.copy()
+            l[i] = ["jmp", val]
+        e = execute(l)
+        if e[0]:
+            print(e[1])
+            break
+
+if __name__ == '__main__':
+    main()
