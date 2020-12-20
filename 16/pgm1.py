f94850ad02ffe9a4437ae3717bfdc99116245fbd
Philippe Chataignon <philippe.chataignon@gmail.com>
2020-12-20 11:26:44 +0100

    16.1

--- 16/pgm1.py
+++ 16/pgm1.py
@@ -4,16 +4,15 @@ import re
 def test_ticket(liste_t, d):
     ret = []
     for t in liste_t:
-        for s1, e1, s2, e2 in d.values():
-            for e in t:
-                print(e, s1, e1, s2, e2)
-                if not (s1 <= e <= e1 or s2 <= e <= e2):
-                    ret.append(e)
+        for e in t:
+            w = [not (s1 <= e <= e1 or s2 <= e <= e2) for s1, e1, s2, e2 in d.values()]
+            if all(w):
+                ret.append(e)
     return ret
 
 hdr = re.compile(r'^(.*): (\d+)-(\d+) or (\d+)-(\d+)$')
 
-with open("test.txt") as f:
+with open("input.txt") as f:
     ll = f.read()[0:-1].split("\n\n")
 
 d = {}
@@ -29,4 +28,3 @@ near = [eval("[" + e.strip() + "]") for e in ll[2].split("\n")[1:]]
 t = test_ticket(near, d)
 print(t)
 print(sum(t))
-
